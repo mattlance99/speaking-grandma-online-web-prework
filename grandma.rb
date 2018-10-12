bd52b851921bc9e4beb07f0b9c2ee8106a9527b3
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-12 20:50:22 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
   pumpkin_response = "I LOVE YOU TOO PUMPKIN!"
+  cant_hear_response = 
   if (phrase.upcase == phrase) && (phrase ==  "I LOVE YOU GRANDMA!") 
     puts "I LOVE YOU TOO PUMPKIN!"
     pumpkin_response
-  elseif 
+  elseif phrase.upcase !== phrase
     puts "HUH?! SPEAK UP SONNY!"
   end
 end
