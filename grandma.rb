8dc5e03b0dcfce91f3f177dc4daa93ca0a8ea49e
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-12 20:49:00 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -12,11 +12,11 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
-  response = "I LOVE YOU TOO PUMPKIN!"
+  pumpkin_response = "I LOVE YOU TOO PUMPKIN!"
   if (phrase.upcase == phrase) && (phrase ==  "I LOVE YOU GRANDMA!") 
     puts "I LOVE YOU TOO PUMPKIN!"
-    response
-  else
+    pumpkin_response
+  elseif 
     puts "HUH?! SPEAK UP SONNY!"
   end
 end
